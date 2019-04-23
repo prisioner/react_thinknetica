41e4c35743291a585273b4e261ea93075dfa95e2
Aleksandr Klimenkov <prisioner@mail.ru>
2019-04-23 10:16:23 +0300

    add Calc module

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ import ReactDOM from 'react-dom';
 
 import HelloWorld from '~/src/components/HelloWorld';
 
+import Calc from '~/src/modules/Calc';
+
 class App extends Component {
   render() {
     return(
-      <HelloWorld />
+      <div>
+        <HelloWorld />
+        <div>2 + 3 = {Calc.sum(2, 3)}</div>
+        <div>2 - 3 = {Calc.subtract(2, 3)}</div>
+        <div>2 * 3 = {Calc.multiply(2, 3)}</div>
+        <div>3 / 2 = {Calc.divide(3, 2)}</div>
+        <div>3 / 0 = {Calc.divide(3, 0)}</div>
+      </div>
     );
   }
 }
